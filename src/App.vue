9ea57d4fd4840d1088b15bbf45ab0477f6f10878
viniciu <viniciuscesarlemes.com>
2025-03-24 20:15:47 -0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,11 @@
 <div class="overflow-hidden">
     <transition name="slide-rigth">
         <div v-if="isLoading == true" class="overflow-hidden">
-            <div class="flex justify-between md:px-14 px-2 py-4">
-                <div class="flex items-end">
-                    <Icon icon="ci:coffee" class="text-white w-10 h-10"/>
-                    <div class="text-white text-3xl ml-2 font-semibold">
-                        vini
-                    </div>
-                </div>
-                <div class="flex">
-                    <a href="https://www.linkedin.com/in/viniciuscesarlemes/" target="_blank">
-                            <Icon icon="mdi:linkedin" class="text-white link w-10 hover:text-[#810c00] h-10"/>
-                    </a>
-                    <a href="https://www.instagram.com/_viniciuslemes/" target="_blank">
-                        <Icon icon="icon-park-outline:instagram" class="text-white link hover:text-[#810c00] w-10 h-10"/>
-                    </a>
-                    <a href="https://github.com/11vinicius" target="_blank">
-                        <Icon icon="mdi:github" class="text-white link hover:text-[#810c00]   w-10 h-10"/>
-                    </a>
-                </div>
-            </div>
-            <div class="flex overflow-hidden justify-between" >
-                <div class="md:w-6/12 md:px-14 px-4 md:relative py-20">
-                    <div class="bg-[#810c00] rounded-full bola w-48 h-48"></div>
-                    <div class="absolute top-28 md:left-14">
-                        <div class="text-white text-3xl font-semibold mt-24 md:mt-0">
-                            Do Conceito à Implementação Desenvolvimento Full-Stack
-                        </div>
-                        <div class="text-white">
-                            Com experiência em Vue.js, Laravel, e Flutter, eu transformo suas ideias em aplicativos e sistemas funcionais. Desde a criação de interfaces intuitivas até o desenvolvimento de backends robustos, minha missão é fornecer soluções técnicas que atendam às suas necessidades e superem suas expectativas.
-                        </div>
-                    </div>
-                </div>
-                <div class="hidden md:block">
-                    <video class="h-84 w-[600px] ml-44" autoplay loop muted>
-                        <source src="../public/assets/video.mp4" type="video/mp4">
-                        Seu navegador não suporta a tag de vídeo.
-                    </video>
-                </div>
-            </div>
+          
+            <video autoplay loop muted playsinline class="absolute top-0 left-0 w-full h-full object-cover">
+                <source src="../assets/video.mp4" type="video/mp4">
+                Seu navegador não suporta vídeos.
+            </video>
                 
             <div class="grid md:grid-cols-4 gap-4 px-4 pt-8  md:px-14 pb-24 overflow-hidden" >
                 <div class="p-6 shadow-white border border-white backdrop-blur-md rounded-lg shadow-lg">
